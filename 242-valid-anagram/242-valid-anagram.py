946f7af4f0e7f8e08bc34733b439edcad589b9af
kamiwis <92603161+kamiwis@users.noreply.github.com>
2022-09-01 14:31:53 -0400

    Time: 71 ms (60.59%), Space: 14.4 MB (67.18%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,7 +1,9 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        #created counter for each str to count occurences of each character
         s_counter = Counter(s)
         t_counter = Counter(t)
+        #return comparison of counters
         return s_counter == t_counter
         
